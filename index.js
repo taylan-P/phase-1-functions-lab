007e3cba96bd27b6d14fb4cfe47486329299f139
TaylanPostalci <tay.postal@gmail.com>
2022-09-19 19:51:49 -0400

    functionLab

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Code your solution in this file!
+//const hq = 42;
+//let x = distanceFromHqInBlocks();
+function distanceFromHqInBlocks(y){
+ return Math.abs(y-42);
+}
+
+function distanceFromHqInFeet(n){
+    return distanceFromHqInBlocks(n) * 264;
+    
+   
+ 
+    
+}
+function distanceTravelledInFeet(s,d){
+let k = Math.abs(s - d) * 264;
+
+return k;
+}
+
+function calculatesFarePrice(s,d){
+let k = distanceTravelledInFeet(s,d)
+if (k <= 400) {
+    return 0
+
+
+ } else if (k >= 401 && k <=2000) {
+
+
+    return (k - 400)*.02; 
+
+    } else if (k >= 2001 && k <=2500) {
+        return 25
+
+    } else (k >= 2500) 
+        return "cannot travel that far"
+    
+
+}
+
+
+
+//console.log(difference)
+// const distanceFromHqInBlocks = x;
+// const feetInBlocks = 264;
+// const multiply = distanceFromHqInBlocks * feetInBlocks;
+// return multiply;
